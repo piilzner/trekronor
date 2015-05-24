ceb69d5c8130dda0aa8ef57a7b7c5232174ae659
piilzner <nils.lofgren@hotmail.com>
2015-05-24 02:53:11 +0200

    går som smooooaarrt

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,59 @@
 $(document).ready(function(){
 
-	console.log("hej");
+	var start_game_music = new Audio('assets/lib/sounds/NHLintro.mp3');
+	var page_intro = new Audio('assets/lib/sounds/pageintro.mp3');
+
+	// Start game music
+	page_intro.play();
+
+
+	// start first animation
+	// make items appear
+	$('.select-players').click(function(){
+
+		$('.players').css("transform", "scale(1.4)");
+		$('#start-game').css("display", "flex");
+
+	});
+
+
+	// Start game 
+	// Jumbotrone appears
+	$('#start-game').click(function(){
+
+		// if one of inputs is empty Alert else send forward
+		if($('#p1').val().length == 0 || $('#p2').val().length == 0){
+			
+			alert("Enter a playername");
+		
+		}else{		
+
+			$('.select-players').css("margin", "0 300px");
+			$('.jumbotrone').css("top", "300px");
+			$('#start-game').css("display", "none");
+
+			// Pause intro music
+			page_intro.pause();
+
+			// NHL intro sound starts
+			start_game_music.play();
+		}
+	});
+
+
+	// Control settings
+	$('.control-btn').click(function(){
+
+		if($('.controls').css("display") == "none"){
+
+			$('.controls').css("display", "block");
+
+		}else{
+
+			$('.controls').css("display", "none");
+
+		}
+	});
+
 
 });
---
+++ assets/js/mousetrap.js
@@ -0,0 +1,65 @@
+
+// Change active user player1 / player2
+Mousetrap.bind('k', changeActiveUser);
+
+// Add a goal
+Mousetrap.bind('g', goal);
+
+
+
+// Change active user player1 / player2
+// Change scoreboard, Home and away
+function changeActiveUser(){
+
+	if(document.getElementById('player-one').classList.contains('active')){
+
+		document.getElementById('player-one').classList.remove('active');
+		document.getElementById('player-two').classList.add('active');
+
+	}else{
+
+		document.getElementById('player-two').classList.remove('active');
+		document.getElementById('player-one').classList.add('active');
+	}
+	
+	if(document.getElementById('home-score').classList.contains('score')){
+
+		document.getElementById('home-score').classList.remove('score');
+		document.getElementById('away-score').classList.add('score');
+
+	}else{
+
+		document.getElementById('away-score').classList.remove('score');
+		document.getElementById('home-score').classList.add('score');
+
+	}
+
+};
+
+
+
+
+var homeGoals = 0;
+var awayGoals = 0;
+var audio = new Audio('assets/lib/sounds/goal.mp3');
+
+
+// Update goals home and away 
+// Add sound when goal scores
+function goal(){
+
+	if(document.getElementById('home-score').classList.contains('score')){
+
+		homeGoals++;
+		document.getElementById('home-score').innerHTML = homeGoals;
+		audio.play();
+
+	}else{
+
+		awayGoals++;
+		document.getElementById('away-score').innerHTML = awayGoals;
+		audio.play();
+	}
+
+};
+
